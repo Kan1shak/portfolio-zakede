99d94c8fc512c61c401b8f4339c316c6532e12fa
Kan1shak <112748208+Kan1shak@users.noreply.github.com>
2024-03-15 00:57:17 +0530

    Change logo image and add favicon

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,6 +1,6 @@
-const contactImage = document.querySelector('.contact-image img');
-if (isMobile()) {
-    contactImage.src = '/images/Contact_Me.png';
-} else {
-    contactImage.src = '/images/Contact_Me.gif';
-}
+// const contactImage = document.querySelector('.contact-image img');
+// if (isMobile()) {
+//     contactImage.src = '/images/Contact_Me.png';
+// } else {
+//     contactImage.src = '/images/Contact_Me.gif';
+// }
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,8 +2,8 @@ const projectItems = document.querySelectorAll('.projects-item');
 
 projectItems?.forEach(addQuickView);
 
-if (isMobile()){
-    document.querySelector('header img').src = '/images/Website_Top_Animation.png';
-} else {
-    document.querySelector('header img').src = '/images/header.gif';
-}
+// if (isMobile()){
+//     document.querySelector('header img').src = '/images/Website_Top_Animation.png';
+// } else {
+//     document.querySelector('header img').src = '/images/header2.png';
+// }
